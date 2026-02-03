a7cb4f7f50115b30691ff703761589ba7223fd05
Borisb127 <boris.borochov92@gmail.com>
2026-02-03 18:36:02 +0200

    Finish Task 1 - Added confirm to remove location

--- ex/travel-tip-starter/js/app.controller.js
+++ ex/travel-tip-starter/js/app.controller.js
@@ -69,6 +69,9 @@ function renderLocs(locs) {
 }
 
 function onRemoveLoc(locId) {
+    const isConfirmed = confirm('Are you sure you want to remove this location?')
+    if (!isConfirmed) return
+
     locService.remove(locId)
         .then(() => {
             flashMsg('Location removed')
